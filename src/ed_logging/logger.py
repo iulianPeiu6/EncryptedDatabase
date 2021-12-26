154cd6b419bcb547cab2f5e4cb4ce0f7466eabb0
Iulian Peiu <iulianpeiu6@gmail.com>
2021-12-27 00:40:38 +0200

    Doc logging package

--- src/ed_logging/logger.py
+++ src/ed_logging/logger.py
@@ -1,12 +1,21 @@
+"""Logger
+"""
+
 from enum import Enum
 
 
 class Environment(Enum):
+    """Stores an enumeration of the possible environments: Production or Development.
+    """
+
     DEVELOPMENT = 0
     PRODUCTION = 1
 
 
 class Color:
+    """Stores an enumeration of the colors used for logger customization.
+    """
+
     BLUE = '\033[94m'
     GREEN = '\033[92m'
     RED = '\033[91m'
@@ -14,26 +23,56 @@ class Color:
 
 
 class LogLevel(str, Enum):
+    """Stores an enumeration of the possible logging levels.
+    """
     INFO = "INFO"
     DEBUG = "DEBUG"
     ERROR = "ERROR"
 
 
 class Logger(object):
+    """Logger model.
+    The logging  is done differently regarding the environment where is running:
+    Production or Development.
+
+    In production the debug is disabled and the info and error tags are ignored
+    and only the message is printed.
+    """
+
     def __init__(self, env):
+        """Constructor
+
+        :param env: the environment where the application is running.
+        """
         self._env = env
 
     def info(self, msg):
+        """Log a message as a info.
+
+        :param msg: the log message
+        """
+
         if self._env == Environment.DEVELOPMENT:
             print(f"{Color.GREEN}{LogLevel.INFO}{Color.BASE} \t{msg}")
         else:
             print(msg)
 
     def debug(self, msg):
+        """Log a message as a debug.
+
+        :param msg: the log message
+        """
+
         if self._env == Environment.DEVELOPMENT:
             print(f"{Color.BLUE}{LogLevel.DEBUG}{Color.BASE} \t{msg}")
 
     def error(self, msg, error):
+        """Log a message as a error.
+
+        :param msg: the log message
+        :param error: the associated error
+        """
+
         if self._env == Environment.DEVELOPMENT:
             print(f"{Color.RED}{LogLevel.ERROR}{Color.BASE} \t{msg}:'{error}'")
         else:
